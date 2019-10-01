db1c413c993865ce867a133fe8390ad8900e29bf
Shequana <ShequanaGarnett@gmail.com>
2019-10-01 13:50:18 -0400

    update

--- Control/Assets/Scripts/Eat_Food.cs
+++ Control/Assets/Scripts/Eat_Food.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Eat_Food : MonoBehaviour
 {
+    Rigidbody myRb;
+    public float multiplier = 10f;
+    int Prey_Eaten;
+    public int eatThresh = 3;
+    private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody>();
+        rend = GetComponent<Renderer>();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    void OnCollisionEnter(Collision col)
     {
-        
+        if (col.gameObject.CompareTag("Prey"))
+        {
+            //Debug.Log("The preditor hit you.");
+            Destroy(col.gameObject);
+            Prey_Eaten++;
+
+        }
+
+        if (col.gameObject.CompareTag("Preditor"))
+        {
+            if (Prey_Eaten < eatThresh)
+            {
+                Destroy(gameObject);
+                rend.material.color = Color.red;
+            }
+        }
     }
 }
