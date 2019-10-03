88457dc87956bf799a4d66a381a9b61222403d25
Student <student@v314-ent-006.local>
2019-10-03 15:47:05 -0400

    update

---
+++ Control_New/Assets/Scripts/BallPoints.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPoints : MonoBehaviour
+{
+    public AudioSource collectSound;
+    public GameObject Preditor;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Bounce")
+        { keepScore.Score += 1;
+            Debug.Log("We got hit");
+            Destroy(gameObject);
+            collectSound.Play();
+        }
+    }
+}
---
+++ Control_New/Assets/Scripts/CollectBalls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectBalls : MonoBehaviour
+{
+    public GameObject scoreText;
+    public int theScore;
+    public AudioSource collectSound;
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Preditor"))
+        {
+            collectSound.Play();
+            //theScore +=1;
+            //scoreText.GetComponent<Text>().text = "SCORE" + theScore;
+            Destroy(gameObject);
+        }
+    }
+
+}
+
