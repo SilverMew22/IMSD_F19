dcd41f26d43c1c2123c3ff05ea738550e1ca869e
Mohamed <only1hamada@live.com>
2019-11-18 11:43:10 -0500

    eightboxes

---
+++ MTEC-2120-Interactive-Media-Systems-Design/Assets/Big_Small.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Big_Small : MonoBehaviour
+{
+    Transform[] boxes = new Transform[3];
+    public bool large;
+    public GameObject prefabOfSphere;
+    public int x = 1;
+    public int y = 1;
+    public int z = 1;
+    
+    void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject g = Instantiate(prefabOfSphere, new Vector3(-8 + 2 * i, 0, 0), Quaternion.identity);
+            boxes[i] = g.transform;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        BigOrSmall(boxes[0], Input.GetKey(KeyCode.A));
+        BigOrSmall(boxes[1], Input.GetKey(KeyCode.S));
+        BigOrSmall(boxes[2], Input.GetKey(KeyCode.D));
+       
+    }
+    void BigOrSmall(Transform box, bool large)
+    {
+        Vector3 hold = box.transform.localScale;
+        if (large)
+             transform.localScale += new Vector3(x,y,z);
+        else
+             transform.localScale -= new Vector3(x,y,z); 
+        box.transform.localScale = hold;
+    }
+}
---
+++ MTEC-2120-Interactive-Media-Systems-Design/Assets/Scripts/GetWeather.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class GetWeather : MonoBehaviour
+{
+    string key = "49fd35a8da2650013f629c4d0fce42f8";
+    void Start()
+    {
+        StartCoroutine(AskForWeather());
+    }
+    IEnumerator AskForWeather()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("http://api.openweathermap.org/data/2.5/weather?zip=11238&mode=xml&APPID=" + key);
+        Debug.Log("Request sent, pending response...");
+        yield return www.SendWebRequest();
+        if (!www.isNetworkError && !www.isHttpError)
+        {
+            // Get text content like this:
+            Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Debug.Log(www.error + " " + www);
+        }
+    }
+}
